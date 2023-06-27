1508b7cb8e36b662d4d2fcf7f9e3b35387113eb7
thilaks-hash <thilagavathyr025@gmail.com>
2023-06-27 14:11:49 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,30 @@
-import React from 'react'
-import '../styles/App.css';
-
-
+import React from "react";
+import "../styles/App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = { date: new Date() };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(
-// write your code here
- 
-    );
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
   tick() {
     this.setState({
-      date: new Date()
+      date: new Date(),
     });
-    
   }
 
   render() {
     return (
-      <div id='main'>
+      <div id="main">
         <h1>Welcome to Newton School</h1>
-        <div id='timer'>{this.state.date.toLocaleTimeString()}</div>
+        <div id="timer">{this.state.date.toLocaleTimeString()}</div>
       </div>
     );
   }
 }
-
-
 
 export default App;
